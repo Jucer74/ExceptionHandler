e37ddaeb0420bd14421b40f18f7d5c772d490008
Julio Robles <Julio.Robles@GMFinancial.com>
2021-04-21 01:12:35 -0500

    Add ExceptionHandler

---
+++ Source/ExceptionHandler/WebApi/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,56 @@
+﻿using Application.Exceptions;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace WebApi.Middleware
+{
+    /// <summary>
+    /// Handler the exeptions
+    /// </summary>
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next ?? throw new ArgumentNullException(nameof(next));
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            const string BUSINESSEXCEPTION_CONTROLLED_MESSAGE = "BusinessException controlled";
+            const string EXCEPTION_CONTROLLED_MESSAGE = "Exception not controlled";
+            const string HASSTARTED_MESSAGE = "The response has already started, middleware will not be executed";
+
+            if (httpContext == null)
+            {
+                return;
+            }
+
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                if (httpContext.Response.HasStarted)
+                {
+                    //Log.Warning(HASSTARTED_MESSAGE);
+                    throw;
+                }
+
+                string logMessageException = EXCEPTION_CONTROLLED_MESSAGE;
+                if (ex.GetBaseException() is BusinessException)
+                {
+                    ex = ex.GetBaseException();
+                    logMessageException = BUSINESSEXCEPTION_CONTROLLED_MESSAGE;
+                }
+
+                await httpContext.HandleExceptionAsync(ex);
+                //Log.Warning(ex, logMessageException);
+                //Log.Error(ex, ex.Message);
+            }
+        }
+    }
+}
--- Source/ExceptionHandler/WebApi/Startup.cs
+++ Source/ExceptionHandler/WebApi/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using WebApi.Middleware;
 
 namespace WebApi
 {
@@ -38,6 +39,8 @@ namespace WebApi
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1"));
             }
+
+            app.UseExceptionMiddleware();
 
             app.UseHttpsRedirection();
 
