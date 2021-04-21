f5748294683b7e94f1f8f98b231dfdbfd65d715f
Julio Robles <Julio.Robles@GMFinancial.com>
2021-04-21 00:48:27 -0500

    Initial Version

---
+++ Source/ExceptionHandler/Infrastructure/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
